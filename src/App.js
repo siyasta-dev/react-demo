657d2cb3db08d62411279e577289a967414fb27a
Vivek Saurabh <vivek.saurabh@siemens-healthineers.com>
2022-12-15 02:55:35 +0530

    table changes complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useMemo } from 'react';
+import MaterialReactTable from 'material-react-table';
+import data from './data'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+function getMonths(data){
+  return [... new Set(data.map(cust => cust.data.map(dta => dta.month).concat(getMonths(cust.childs))).flat())];  
+};
+
+const App = () => {
+  let monthHeaders = getMonths(data).map(month => {
+    return {
+      header: month,
+      columns: [
+        {
+          header: 'NPS',
+          id: month + 'nps',
+          accessorFn: (row) => {
+            let monthData = row.data.find(dt => dt.month === month)
+            return monthData ? monthData.nps : '-';
+          },
+        },
+        {
+          header: 'PPF',
+          id: month + 'ppf',
+          accessorFn: (row) => {
+            let monthData = row.data.find(dt => dt.month === month)
+            return monthData ? monthData.ppf : '-';
+          }
+        },
+        {
+          header: 'DIY',
+          id: month + 'diy',
+          accessorFn: (row) => {
+            let monthData = row.data.find(dt => dt.month === month)
+            return monthData ? monthData.diy : '-';
+          }
+        }
+      ]
+    }
+  });
+  
+  const columns = useMemo(
+    () => [
+      {
+        accessorKey: 'name',
+        header: 'Company',
+      },
+      ...monthHeaders
+    ],
+    [],
   );
-}
+
+  return <MaterialReactTable columns={columns} data={data} enableExpanding getSubRows={(originalRow) => originalRow.childs}/>;
+};
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,138 @@
+const data = [
+    {
+      name: "Alphabet",
+      data: [
+        {
+          month: "Jan 2020",
+          nps: 20.7,
+          ppf: 15.9,
+          diy: 14.1
+        },
+        {
+          month: "Feb 2020",
+          nps: 76.7,
+          ppf: 97.9,
+          diy: 74.1
+        },
+        {
+          month: "Mar 2020",
+          nps: 10.7,
+          ppf: 66.9,
+          diy: 54.1
+        },
+        {
+          month: "Apr 2020",
+          nps: 88.7,
+          ppf: 24.9,
+          diy: 92.1
+        },
+      ],
+      childs: [
+        {
+          name: "Google",
+          data: [
+            {
+              month: "Jan 2020",
+              nps: 25.7,
+              ppf: 50.9,
+              diy: 19.1
+            },
+            {
+              month: "Feb 2020",
+              nps: 78.7,
+              ppf: 54.9,
+              diy: 91.1
+            },
+          ],
+          childs: []
+        },
+        {
+          name: "Youtube",
+          data: [
+            {
+              month: "Jan 2020",
+              nps: 22.7,
+              ppf: 55.9,
+              diy: 77.1
+            },
+            {
+              month: "Feb 2020",
+              nps: 64.7,
+              ppf: 71.9,
+              diy: 83.1
+            },
+          ],
+          childs: []
+        },
+      ]
+    },
+    {
+      name: "Meta",
+      data: [
+        {
+          month: "Jan 2020",
+          nps: 59.7,
+          ppf: 32.9,
+          diy: 37.1
+        },
+        {
+          month: "Feb 2020",
+          nps: 16.7,
+          ppf: 48.9,
+          diy: 34.1
+        },
+        {
+          month: "Mar 2020",
+          nps: 64.7,
+          ppf: 89.9,
+          diy: 31.1
+        },
+        {
+          month: "Apr 2020",
+          nps: 55.7,
+          ppf: 33.9,
+          diy: 14.1
+        },
+      ],
+      childs: [
+        {
+          name: "Facebook",
+          data: [
+            {
+              month: "Jan 2020",
+              nps: 47.7,
+              ppf: 62.9,
+              diy: 34.1
+            },
+            {
+              month: "Feb 2020",
+              nps: 66.7,
+              ppf: 22.9,
+              diy: 95.1
+            },
+          ],
+          childs: []
+        },
+        {
+          name: "WhatsApp",
+          data: [
+            {
+              month: "Jan 2020",
+              nps: 16.7,
+              ppf: 48.9,
+              diy: 96.1
+            },
+            {
+              month: "Feb 2020",
+              nps: 36.7,
+              ppf: 69.9,
+              diy: 28.1
+            },
+          ],
+          childs: []
+        },
+      ]
+    }
+  ];
+
+  export default data
