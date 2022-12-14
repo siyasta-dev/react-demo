f149ecbe7271ceb3ca42a0b88702d25b739e78e1
Vivek Saurabh <vivek.saurabh@siemens-healthineers.com>
2022-12-15 04:46:22 +0530

    fixed warnings

--- src/MaterialTable.js
+++ src/MaterialTable.js
@@ -3,41 +3,11 @@ import MaterialReactTable from 'material-react-table';
 import data from './data'
 
 function getMonths(data){
-  return [... new Set(data.map(cust => cust.data.map(dta => dta.month).concat(getMonths(cust.childs))).flat())];  
+  return [...new Set(data.map(cust => cust.data.map(dta => dta.month).concat(getMonths(cust.childs))).flat())];  
 };
 
 const MaterialTable = () => {
-  let monthHeaders = getMonths(data).map(month => {
-    return {
-      header: month,
-      columns: [
-        {
-          header: 'NPS',
-          id: month + 'nps',
-          accessorFn: (row) => {
-            let monthData = row.data.find(dt => dt.month === month)
-            return monthData ? monthData.nps : '-';
-          },
-        },
-        {
-          header: 'PPF',
-          id: month + 'ppf',
-          accessorFn: (row) => {
-            let monthData = row.data.find(dt => dt.month === month)
-            return monthData ? monthData.ppf : '-';
-          }
-        },
-        {
-          header: 'DIY',
-          id: month + 'diy',
-          accessorFn: (row) => {
-            let monthData = row.data.find(dt => dt.month === month)
-            return monthData ? monthData.diy : '-';
-          }
-        }
-      ]
-    }
-  });
+  //let monthHeaders = ;
   
   const columns = useMemo(
     () => [
@@ -45,7 +15,37 @@ const MaterialTable = () => {
         accessorKey: 'name',
         header: 'Company',
       },
-      ...monthHeaders
+      ...getMonths(data).map(month => {
+        return {
+          header: month,
+          columns: [
+            {
+              header: 'NPS',
+              id: month + 'nps',
+              accessorFn: (row) => {
+                let monthData = row.data.find(dt => dt.month === month)
+                return monthData ? monthData.nps : '-';
+              },
+            },
+            {
+              header: 'PPF',
+              id: month + 'ppf',
+              accessorFn: (row) => {
+                let monthData = row.data.find(dt => dt.month === month)
+                return monthData ? monthData.ppf : '-';
+              }
+            },
+            {
+              header: 'DIY',
+              id: month + 'diy',
+              accessorFn: (row) => {
+                let monthData = row.data.find(dt => dt.month === month)
+                return monthData ? monthData.diy : '-';
+              }
+            }
+          ]
+        }
+      })
     ],
     [],
   );
