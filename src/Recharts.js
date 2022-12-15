0a2b4f3f9c92e819c08a73d0931fdf3ea563aecd
Vivek Saurabh <saurabhvivek30@gmail.com>
2022-12-16 02:09:58 +0530

    formatted legend to uppercase

--- src/Recharts.js
+++ src/Recharts.js
@@ -47,7 +47,7 @@ export default function RenderLineChart() {
         <XAxis dataKey="month" strokeDasharray="3 3" />
         <YAxis yAxisId="left" orientation="left" strokeDasharray="3 3" tickFormatter={formatter} tickLine={false} />
         <YAxis yAxisId="right" orientation="right" strokeDasharray="3 3" tickFormatter={formatter} tickLine={false} />
-        <Legend />
+        <Legend formatter={(value) => value.toUpperCase()}/>
         <Bar yAxisId="right" dataKey="ppf" fill="#8884d8" label={{position: 'top', formatter: formatter}}/>
         <Line yAxisId="left" type="monotone" dataKey="diy" stroke="#82ca9d" />
         <Line yAxisId="left" type="monotone" dataKey="nps" stroke="#ff7300" />
